4fefae9242967b58a4af5ce001df1d86e105b018
chenrengui <chenrengui@xiangshang360.com>
2018-09-17 11:07:22 +0800

    orm

--- www/orm.py
+++ www/orm.py
@@ -16,7 +16,7 @@ def create_pool(loop, **kw):
         host=kw.get('host', 'localhost'),
         port=kw.get('port', 3306),
         user=kw['user'],
-        password = kw['password'],
+        password=kw['password'],
         db=kw['db'],
         charset=kw.get('charset', 'utf8'),
         autocommit=kw.get('autocommit', True),
@@ -30,7 +30,7 @@ def create_pool(loop, **kw):
 @asyncio.coroutine
 def select(sql, args, size=None):
     log(sql, args)
-    global  __pool
+    global __pool
     with (yield from __pool) as conn:
         cur = yield from conn.cursor(aiomysql.DictCursor)
         yield from cur.execute(sql.replace('?', '%s'), args or ())
@@ -39,7 +39,7 @@ def select(sql, args, size=None):
         else:
             rs = yield from cur.fetchall()
         yield from cur.close()
-        logging.info('Rows returned:%s' %len(rs))
+        logging.info('Rows returned:%s' % len(rs))
         return rs
 
 
@@ -62,3 +62,53 @@ def execute(sql, args):
 def log(sql, args):
     logging.info('SQL: %s, args: %s' % (sql, args))
 
+
+# ---------------------
+
+
+# 定义Model
+class Model(dict, metaclass=ModelMetaclass):
+    def __init__(self, **kw):
+        super(Model, self).__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Model' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def getValue(self, key):
+        return getattr(self, key, None)
+
+    def getValuOrDefault(self, key):
+        value = getattr(self, key, None)
+        if value is None:
+            field = self.__mappings__[key]
+            if field.default is not None:
+                value = field.default() if callable(field.default) else field.default
+                logging.debug('Using dfeault value for %s:%s' % (key, str(value)))
+                setattr(self, key, value)
+        return value
+
+
+class Field(object):
+    def __init__(self, name, column_type, primary_key, default):
+        self.name = name
+        self.column_type = column_type
+        self.primary_key = primary_key
+        self.default = default
+
+    def __str__(self):
+        return '<%s, %s, %s>' % (self.__class__.__name__, self.column_type, self.name)
+
+
+class StringField(Field):
+    def __init__(self, name = None, primary_key = False, default = None, ddl='varchar(100)'):
+        super().__init__(name, ddl, primary_key, default)
+
+
+class IntegerField(Field):
+    pass
