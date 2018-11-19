973bc352bd8c3fb2cdc19baabd52a163406000de
chenrengui <chenrengui@xiangshang360.com>
2018-11-19 22:37:36 +0800

    html templates

--- www/handlers.py
+++ www/handlers.py
@@ -1,13 +1,21 @@
 # -*- coding:utf-8 -*-
 
-from models import User
+from models import User, Blog
 from coroweb import get, post
+
+import time
 
 
 @get('/')
 async def index():
-    users = await User.findall()
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, ' \
+              'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
+    ]
     return {
-        '__template__': 'test.html',
-        'users': users
+        '__template__': 'blogs.html',
+        'blogs': blogs
     }
